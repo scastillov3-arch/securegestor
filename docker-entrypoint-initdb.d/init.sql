14015f8f43d3eed298e02c4d5921ea5f29f3ef93
Saraí Elisabet Castillo Valenzuela <elisabetcv@Mac.lan>
2025-10-24 21:24:58 -0600

    Implementación CI/CD, init.sql y actualización Docker Compose

---
+++ docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,5 @@
+CREATE USER securegestor_user WITH ENCRYPTED PASSWORD '3cur3G3st0r_2025!';
+CREATE DATABASE securegestor_db;
+GRANT ALL PRIVILEGES ON DATABASE securegestor_db TO securegestor_user;
+
+
